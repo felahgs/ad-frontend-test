f27869d2462433e1de7baa1ee91c144c64b67f46
Felipe Souza <fgsouza93@gmail.com>
2025-01-02 17:02:55 -0300

    feat: create Footer component

--- src/components/Footer/Footer.test.tsx
+++ src/components/Footer/Footer.test.tsx
@@ -3,10 +3,10 @@ import { render, screen } from "@testing-library/react";
 import Footer from "./Footer";
 
 describe("Footer", () => {
-  it("should render properly", async () => {
-    render(<Footer />);
+  it("should render properly", () => {
+    render(<Footer>Footer</Footer>);
 
-    const text = await screen.findByText("Footer");
+    const text = screen.getByText("Footer");
 
     expect(text).toBeInTheDocument();
   });
--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,7 +1,21 @@
-import React from "react";
+import clsx from "clsx";
+import React, { HTMLProps, ReactNode } from "react";
 
-function Footer() {
-  return <div>Footer</div>;
+export interface FooterProps extends HTMLProps<HTMLElement> {
+  children?: ReactNode;
+}
+
+function Footer({ children, className }: FooterProps) {
+  return (
+    <footer
+      className={clsx(
+        "flex items-center justify-center h-44 bg-neutral-700 text-white",
+        className,
+      )}
+    >
+      {children}
+    </footer>
+  );
 }
 
 export default Footer;
