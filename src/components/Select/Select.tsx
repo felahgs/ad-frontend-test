1912d8eb21e9556e384971ca652cc1f19b1ff5d8
Felipe Souza <fgsouza93@gmail.com>
2025-01-02 17:35:15 -0300

    feat: create Select component

--- src/components/Select/Select.tsx
+++ src/components/Select/Select.tsx
@@ -1,7 +1,16 @@
-import React from "react";
+import React, { InputHTMLAttributes } from "react";
 
-function Select() {
-  return <div>Select</div>;
+interface SelectProps extends InputHTMLAttributes<HTMLSelectElement> {}
+
+function Select({ children, className, ...rest }: SelectProps) {
+  return (
+    <select
+      className={`p-2 rounded border-none cursor-pointer outline-zinc-400 ${className}`}
+      {...rest}
+    >
+      {children}
+    </select>
+  );
 }
 
 export default Select;
