71f5bb19765e1234acf2edf118f412572c6ae93f
Felipe Souza <fgsouza93@gmail.com>
2025-01-03 14:52:31 -0300

    feat: add Select index and some minor changes

--- src/components/Select/Select.tsx
+++ src/components/Select/Select.tsx
@@ -1,11 +1,11 @@
 import React, { InputHTMLAttributes } from "react";
 
-interface SelectProps extends InputHTMLAttributes<HTMLSelectElement> {}
+export interface SelectProps extends InputHTMLAttributes<HTMLSelectElement> {}
 
 function Select({ children, className, ...rest }: SelectProps) {
   return (
     <select
-      className={`p-2 rounded border-none cursor-pointer outline-zinc-400 ${className}`}
+      className={`p-2 rounded border-none cursor-pointer outline-zinc-400 text-primary ${className}`}
       {...rest}
     >
       {children}
